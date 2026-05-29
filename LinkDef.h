c6469c9a2d37f367e8ed6c637bde356d2c9ad460
Mariia Mitrankova <mashalario@gmail.com>
2026-05-29 12:37:48 -0400

    Uodated for Full Tracks

--- LinkDef.h
+++ LinkDef.h
@@ -3,5 +3,15 @@
 #pragma link C++ class InModuleTrackv1+;
 #pragma link C++ class InModuleTrackContainer+;
 #pragma link C++ class InModuleTrackContainerv1+;
+#pragma link C++ class InModuleTrack+;
+#pragma link C++ class InModuleTrackv1+;
+#pragma link C++ class InModuleTrackContainer+;
+#pragma link C++ class InModuleTrackContainerv1+;
+
+#pragma link C++ class FullTrack+;
+#pragma link C++ class FullTrackv1+;
+#pragma link C++ class FullTrackContainer+;
+#pragma link C++ class FullTrackContainerv1+;
+
 #pragma link C++ class std::vector<std::pair<unsigned int, unsigned int>>+;
 #endif
